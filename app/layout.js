fc468ad8791034e4924f415ab71c43170bd34522
Sewon Park <william02px2022@gmail.com>
2024-04-19 11:16:34 -0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className + " flex flex-col"}>
+      <body className={inter.className + " flex flex-col overscroll-none"}>
         <Navbar/>
         {children}
         <Footer/>
