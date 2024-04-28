14d3440796e1764fead65b406efee1eeaf479858
Rboan08 <rdoan08@uw.edu>
2024-04-27 17:50:38 -0700

    Footer
    Restructured footer per the design team's hi-fi wireframes

--- app/ui/Footer.tsx
+++ app/ui/Footer.tsx
@@ -4,49 +4,50 @@ import Image from 'next/image';
 import { FaFacebookF, FaInstagram, FaSpotify, FaTumblr, FaXTwitter } from 'react-icons/fa6';
  
 export default function Footer() {
-  return (
-    <footer className="bg-white py-10">
-      <div className="flex justify-between items-center max-w-6xl mx-auto px-4">
-        {/* Left-side: horizontal line or decorative element */}
-        <div className="border-t-2 border-gray-300 w-1/4" />
-
-        {/* Center: logo and address */}
-        
-
-        {/* Right-side: Social Media Icons */}
-        <div className="flex space-x-4 m-5">
-            <a href="https://x.com" className="text-black hover:text-gray-700">
-                <FaXTwitter  />
-            </a>
-            <a href="https://facebook.com" className="text-black hover:text-gray-700">
-                <FaFacebookF />
-            </a>
-            <a href="https://www.instagram.com/rainydawgradio/" className="text-black hover:text-gray-700">
-                <FaInstagram />
-            </a>
-            <a href="https://spotify.com" className="text-black hover:text-gray-700">
-                <FaSpotify />
-            </a>
-            <a href="https://tumblr.com" className="text-black hover:text-gray-700">
-                <FaTumblr />
-            </a>
-        </div>
-
-        {/* Right-side: horizontal line or decorative element */}
-        <div className="border-t-2 border-gray-300 w-1/4" />
-      </div>
-      <div>
-      <div className="text-center flex justify-center items-center flex-col">
-          <Image src="/logo.png" width={70} height={70} alt="Rainy Dawg Radio" className='m-5'/>
-          <p className="text-sm m-5">
-            Husky Union Building Room 131N <br />
-            4001E Stevens Way NE, Seattle, WA 98195
-          </p>
-          <p className="text-xs text-gray-600 mt-4">
-            © Rainy Dawg Radio 2024
-          </p>
-        </div>
-      </div>
-    </footer>
-  );
+    return (
+        <footer className="m-24 p-20">
+            {/* Container controlling all three major components */}
+            <div className='flex flex-row justify-center'>
+                {/* Container with logo and develop tag */}
+                <div className='flex flex-col'>
+                    <a href='/'>
+                        <Image src="/logoexpanded.png" width={300} height={300} alt="Rainy Dawg Radio"
+                        className='py-7'/>
+                    </a>
+                    <p className="text-xs">
+                        © Rainy Dawg Radio 2024 <br/>
+                        Made with &lt;3 by Web Impact UW
+                    </p>
+                </div>
+                {/* Decorative divider */}
+                <div className="bg-black w-0.5 mx-10"></div>
+                {/* Container with icons and address */}
+                <div className='flex flex-col'>
+                    {/* Just icons */}
+                    <div className='flex flex-row justify-end text-3xl space-x-16 py-12'>
+                        <a href="https://x.com" className="text-black hover:text-gray-700">
+                                  <FaXTwitter  />
+                        </a>
+                        <a href="https://facebook.com" className="text-black hover:text-gray-700">
+                            <FaFacebookF />
+                        </a>
+                        <a href="https://www.instagram.com/rainydawgradio/" className="text-black hover:text-gray-700">
+                            <FaInstagram />
+                        </a>
+                        <a href="https://spotify.com" className="text-black hover:text-gray-700">
+                            <FaSpotify />
+                        </a>
+                        <a href="https://tumblr.com" className="text-black hover:text-gray-700">
+                            <FaTumblr />
+                        </a>
+                    </div>
+                    {/* Address component */}
+                    <p className="text-right text-xs">
+                            Husky Union Building Room 131N <br/>
+                            4001E Stevens Way NE, Seattle, WA 98195
+                    </p>
+                </div>
+            </div>
+        </footer>
+    );
 };
