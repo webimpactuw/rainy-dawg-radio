7191b155bca54793bc56ac590d9ee1235c3858e4
sunnyxtian <134821187+sunnyxtian@users.noreply.github.com>
2024-05-09 18:06:18 -0700

    made the navbar bold

--- app/ui/Navbar.tsx
+++ app/ui/Navbar.tsx
@@ -1,18 +1,18 @@
 import Link from 'next/link'
 import Image from 'next/image'
- 
+
 export default function Navbar() {
     return (
         <nav className="flex justify-between items-center pl-10 pr-10 w-full bg-white pt-5 pb-5">
             <Link href="/">
-                <Image src="/logoexpanded.png" width={250} height={250} alt={"Rainy Dawg Radio"}/>
+                <Image src="/logoexpanded.png" height = {200} width = {200} alt={"Rainy Dawg Radio"}/>
             </Link>
 
             <div className="flex space-x-14">
-                <Link href="/about" className="hover:text-gray-700">About</Link>
-                <Link href="/listen" className="hover:text-gray-700">Listen</Link>
-                <Link href="/blog" className="hover:text-gray-700">Blog</Link>
-                <Link href="/gallery" className="hover:text-gray-700">Gallery</Link>
+                <Link href="/about" className="hover:text-gray-700 font-bold">About</Link>
+                <Link href="/listen" className="hover:text-gray-700 font-bold">Listen</Link>
+                <Link href="/blog" className="hover:text-gray-700 font-bold">Blog</Link>
+                <Link href="/gallery" className="hover:text-gray-700 font-bold">Gallery</Link>
             </div>
         </nav>)
 }
