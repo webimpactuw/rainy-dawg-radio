b3b2c4ead169344f082a2d7ea4d03b6315aa8659
Sewon Park <william02px2022@gmail.com>
2024-05-09 18:14:30 -0700

    2 most recent blog post in MainCarousel

--- app/ui/MainCarousel.tsx
+++ app/ui/MainCarousel.tsx
@@ -7,21 +7,20 @@ import Link from "next/link";
 import { PortableText } from "@portabletext/react";
 // import ExampleCarouselImage from 'components/ExampleCarouselImage';
 
-async function getData(page = 0, pageSize = 9) {
-  const query = `*[_type == "post"] | order(publishedAt desc) [0] {
+async function getData() {
+  const query = `*[_type == "post"] | order(publishedAt desc) [0..1] {
     title,
     slug,
     author,
+    summary,
     mainImage {
       asset -> {
         url
       }
     },
-    summary,
-    categories,
     publishedAt,
-    body
   }`
+  
 
   const res = await client.fetch(query);
   return res;
@@ -44,25 +43,41 @@ export default function MainCarousel() {
     <Carousel>
       <div className="flex flex-row p-10">
         <div className="w-1/2 pl-20 self-center">
-            {post ? (<Image src={post.mainImage.asset.url} width={500} height={500} objectFit="cover" alt="Descriptive alt text"/>) : (<></>)}
+            {post ? (<Image src={post[0].mainImage.asset.url} width={500} height={500} objectFit="cover" alt="Descriptive alt text"/>) : (<></>)}
           {/* <Image src="./latestblogsticker.svg" width={300} height={300} alt=""/> */}
         </div>
         <div className="w-1/2 pr-20 self-center">
           <div className="text-3xl font-bold">
-            {post ? (<>{post.title}</>) : (<></>)}
+            {post ? (<>{post[0].title}</>) : (<></>)}
           </div>
           <div className="py-5">
-            {post ? (<>{post.summary}</>) : (<></>)}
+            {post ? (<>{post[0].summary}</>) : (<></>)}
           </div>
           <div>
-            {post ? (<Link href={`/blog/${post.slug.current}`} className="hover:underline">
+            {post ? (<Link href={`/blog/${post[0].slug.current}`} className="hover:underline">
               Read more
             </Link>):(<></>)}
           </div>
         </div>
       </div>
-      <div>
-        Slide 2
+      <div className="flex flex-row p-10">
+        <div className="w-1/2 pl-20 self-center">
+            {post ? (<Image src={post[1].mainImage.asset.url} width={500} height={500} objectFit="cover" alt="Descriptive alt text"/>) : (<></>)}
+          {/* <Image src="./latestblogsticker.svg" width={300} height={300} alt=""/> */}
+        </div>
+        <div className="w-1/2 pr-20 self-center">
+          <div className="text-3xl font-bold">
+            {post ? (<>{post[1].title}</>) : (<></>)}
+          </div>
+          <div className="py-5">
+            {post ? (<>{post[1].summary}</>) : (<></>)}
+          </div>
+          <div>
+            {post ? (<Link href={`/blog/${post[1].slug.current}`} className="hover:underline">
+              Read more
+            </Link>):(<></>)}
+          </div>
+        </div>
       </div>
     </Carousel>
   );
