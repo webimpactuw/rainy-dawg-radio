2a61e864d178032ecc868a17c7da55c24820e52a
Sewon Park <william02px2022@gmail.com>
2024-05-16 18:24:12 -0700

    mobile carousel

--- app/ui/MainCarousel.tsx
+++ app/ui/MainCarousel.tsx
@@ -41,19 +41,19 @@ export default function MainCarousel() {
 
   return (
     <Carousel>
-      <div className="flex flex-row p-10">
-        <div className="w-1/2 pl-20 self-center">
-            {post ? (<Image src={post[0].mainImage.asset.url} className="w-[500px] h-[500px] object-cover" width={500} height={500} objectFit="cover" alt="Descriptive alt text"/>) : (<></>)}
+      <div className="flex flex-col lg:flex-row lg:p-10">
+        <div className="lg:w-1/2 lg:pl-20 self-center">
+            {post ? (<Image src={post[0].mainImage.asset.url} className="w-[300px] h-[300px] lg:w-[500px] lg:h-[500px] object-cover" width={500} height={500} objectFit="cover" alt="Descriptive alt text"/>) : (<></>)}
           {/* <Image src="./latestblogsticker.svg" width={300} height={300} alt=""/> */}
         </div>
-        <div className="w-1/2 pr-20 self-center">
+        <div className="px-20 lg:w-1/2 lg:pr-20 self-center">
           <div className="">
             Blog Post
           </div>
           <div className="large-heading font-bold">
             {post ? (<>{post[0].title}</>) : (<></>)}
           </div>
-          <div className="py-5">
+          <div className="py-5 hidden lg:flex">
             {post ? (<>{post[0].summary}</>) : (<></>)}
           </div>
           <div>
