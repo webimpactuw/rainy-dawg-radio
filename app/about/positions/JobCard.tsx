bd082640c90008f8c599d53143e5a5fa3e6b990d
Rboan08 <rdoan08@uw.edu>
2024-05-20 16:19:09 -0700

    Built schema for JobApps
    Per title

--- app/about/positions/JobCard.tsx
+++ app/about/positions/JobCard.tsx
@@ -1,10 +1,9 @@
-const JobCard = ({ title, desc, link}) => {
+const JobCard = ({ title, desc, link, open}) => {
     return (
         <div className="outline outline-1">
             <header className='py-2 text-center font-semibold bg-rdr-yellow text-3xl outline outline-1'>
                 {title}
             </header>
-
             <div className = "p-6">
                 <p className="text-[0.8rem]">
                     {desc}
--- app/about/positions/TeamApps.tsx
+++ app/about/positions/TeamApps.tsx
@@ -1,22 +1,28 @@
 import Image from 'next/image'
 import Job from './JobCard'
+import { client } from '../../../sanity/lib/client';
+
+
 
 type JobPost = {
     title: string;
     desc: string;
     appUrl: string;
+    open: boolean;
 }
 
 const currJobs: JobPost[] = [
     {
         title: "Volunteer DJ",
         desc: "Being a volunteer DJ means you'd be guaranteed at least hour a week to broadcast your show to the University of Washington campus! Show concepts can range from talk shows, comedy, music niches, and much more. We hire the majority of our DJs in the fall, however DJ applications do open every quarter.",
-        appUrl: "#"
+        appUrl: "#",
+        open: true,
     },
     {
         title: "Volunteer Blawgger",
-        desc: "Blawgers have the opportunity to practice journalism and write about local music in Seattle, such as attending events and listening to music. Rainy Dawg Radio ocasionally supplies bloggers with free tickets to local shows to fuel blogging content and promote local acts. View the blog to get an idea, and break our mold.",
-        appUrl: "#"
+        desc: "Blawggers have the opportunity to practice journalism and write about local music in Seattle, such as attending events and listening to music. Rainy Dawg Radio ocasionally supplies bloggers with free tickets to local shows to fuel blogging content and promote local acts. View the blog to get an idea, and break our mold.",
+        appUrl: "#",
+        open: false,
     }
 ]
 
@@ -28,7 +34,7 @@ const TeamApps = () => {
 
             <div className='grid grid-cols-2 gap-4 px-10 place-content-center' style={{margin:"auto"}}>
                 {currJobs.map((job, index) => (
-                    <Job key={index} title={job.title} desc={job.desc} link={job.appUrl}/>
+                    <Job key={index} title={job.title} desc={job.desc} link={job.appUrl} open={job.open}/>
                 ))}
             </div>
         </div>
