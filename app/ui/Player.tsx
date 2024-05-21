1aac5ccde2d252109109dd4d4c540c87e8de4be4
sunnyxtian <134821187+sunnyxtian@users.noreply.github.com>
2024-05-20 18:57:27 -0700

    added sans serif font and styled player bar

--- app/ui/Player.tsx
+++ app/ui/Player.tsx
@@ -3,7 +3,7 @@ import PlayButton from './PlayButton';
 
 async function getMusic() {
     const res = await fetch('https://spinitron.com/api/spin?count=1&access-token=VIT_hdbWICcgF3nGwvcLJCf6', { next: {revalidate: 30} })
-    
+
     return res.json();
 }
 
@@ -22,7 +22,7 @@ export default async function Player() {
             <div className="flex justify-between items-center px-4 py-2">
                 <div className="flex items-center space-x-2">
                     <div className="w-5 h-5 bg-red-500 rounded-full"></div>
-                    
+
                     <span className="text-center text-lg">Live</span>
                 </div>
                 <div className="flex items-center justify-center space-x-3">
@@ -30,10 +30,10 @@ export default async function Player() {
                     <span>with TEST DJ</span>
                     <span>9:00 am - 10:00 am</span>
                 </div>
-            
+
                 <div className="flex items-center justify-center space-x-3">
                     <Image src={music.items[0].image}  width={61} height={61} alt={"album cover"}/>
-                
+
                     <div className="ml-2">
                         <span className="font-bold">{music.items[0].song}</span>
                     </div>
--- app/ui/PlayerTest.tsx
+++ app/ui/PlayerTest.tsx
@@ -46,22 +46,31 @@ function Player() {
   return (
     <div className="player flex flex-col border-t-2 border-gray bg-white bottom-0 left-0 w-full sticky">
         <div className="flex items-center justify-between px-10 py-2 space-x-6">
-          <section className="flex items-center space-x-2">
-            <div id="liveCircle" className="w-2 h-2 bg-red-500 rounded-full"></div>
-            <span className="text-center text-gray-500">Live</span>
-          </section>
+          <div className="flex items-center space-x-2">
+            <section>
+              <div>
+                {spins && spins.image != "loading" ? (<Image src={spins.image}  width={61} height={61} alt={"album cover"}/>) : (<></>)}
+              </div>
+            </section>
 
-          <section className="flex items-center space-x-2">
-            <div>
-              {spins && spins.image != "loading" ? (<Image src={spins.image}  width={61} height={61} alt={"album cover"}/>) : (<></>)}
-            </div>
-            <div>
-                {spins ? (<span className="font-semibold">{spins.music}</span>) : (<></>)}
-            </div>
-            <div>
-              <p><span className="text-gray-500">by</span> {spins ? (<span>{spins.artist}</span>) : (<></>)}</p>
-            </div>
-          </section>
+            <section>
+              <div className="flex justify-between items-center space-x-2">
+                <p>
+                  {spins ? (<span className="font-mono font-semibold">{spins.music}</span>) : (<></>)}
+                </p>
+                <div id="liveCircle" className="inline w-2 h-2 bg-red-500 rounded-full"></div>
+              </div>
+
+              <div>
+                <p>
+                  <span className="text-gray-500">by </span>
+                  {spins ? (<span>{spins.artist}</span>) : (<></>)}
+                </p>
+              </div>
+            </section>
+          </div>
+
+          <PlayButton />
 
           <section>
             <p className="text-gray-500">Playing on</p>
@@ -70,7 +79,6 @@ function Player() {
             </div>
           </section>
 
-          <PlayButton />
         </div>
     </div>
 
