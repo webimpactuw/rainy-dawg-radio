a0c95d64128ce43c41c47aa3d65290ffae5ff115
blloop <42186044+blloop@users.noreply.github.com>
2025-05-23 11:56:41 -0700

    use next rewrites config for http stream

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                hostname: '**.mzstatic.com'
-            }
-        ],
-        domains: ['cdn.sanity.io', 'spinitron.com'],
-    },
-    async redirects() {
-        return [
-          {
-            source: '/admin/:path*',
-            destination: 'https://rainydawg.sanity.studio',
-            permanent: true,
-          },
-        ]
+  images: {
+    remotePatterns: [
+      {
+        hostname: '**.mzstatic.com'
+      }
+    ],
+    domains: ['cdn.sanity.io', 'spinitron.com'],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/admin/:path*',
+        destination: 'https://rainydawg.sanity.studio',
+        permanent: true,
       },
+    ]
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/audio",
+        destination: "http://166.62.119.4:8000/stream",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
